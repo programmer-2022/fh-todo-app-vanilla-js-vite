3d37673052768f74fd4fa13b45d9b232ce22fbea
Cesar Martinez <km.music92@gmail.com>
2023-09-07 11:19:13 -0500

    add: event listener add todo function

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,17 +1,33 @@
 import html from "./app.component.html?raw";
 import todoStore from "../store/todo.store";
-import { TodoListCSSClass } from "../utils/constantes";
+import { NewTodoInputCSSClass, TodoListCSSClass } from "../utils/constantes";
 import { renderTodos } from "./use-cases";
 
 export const App = (elementID) => {
+  const displayTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    renderTodos(TodoListCSSClass, todos);
+  };
+
   (() => {
     const app = document.createElement("div");
     app.innerHTML = html;
     document.querySelector(elementID).append(app);
 
     todoStore.agregarListaTodosxDefecto();
-    const todos = todoStore.getTodos();
-
-    renderTodos(TodoListCSSClass, todos);
+    displayTodos();
   })();
+
+  // Referencias
+  const newDescriptionInput = document.querySelector(NewTodoInputCSSClass);
+
+  // Listener
+  newDescriptionInput.addEventListener("keyup", ({ key, target }) => {
+    if (key !== "Enter") return;
+    if (target.value.trim().length === 0) return;
+
+    todoStore.addTodo(target.value);
+    displayTodos();
+    target.value = "";
+  });
 };
