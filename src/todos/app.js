c24ba52fee59d8b80a9e1b511ba6c65c9be5968b
Cesar Martinez <km.music92@gmail.com>
2023-09-07 10:48:41 -0500

    update: include renderTodos

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,10 +1,17 @@
-import html from './app.component.html?raw';
+import html from "./app.component.html?raw";
+import todoStore from "../store/todo.store";
+import { TodoListCSSClass } from "../utils/constantes";
+import { renderTodos } from "./use-cases";
 
 export const App = (elementID) => {
-
   (() => {
-    const app = document.createElement('div');
+    const app = document.createElement("div");
     app.innerHTML = html;
     document.querySelector(elementID).append(app);
+
+    todoStore.agregarListaTodosxDefecto();
+    const todos = todoStore.getTodos();
+
+    renderTodos(TodoListCSSClass, todos);
   })();
-}
+};
