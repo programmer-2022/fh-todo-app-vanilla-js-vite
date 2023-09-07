2864f6549b69537570864b3e11dc361dcf8c0bfb
Cesar Martinez <km.music92@gmail.com>
2023-09-07 11:18:44 -0500

    update: color title, add NewTodoInputCSSClass

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -2,20 +2,21 @@ export const createTodoHTML = (todo) => {
   if (!todo) throw new Error("El elemento TODO es requerido");
 
   const isChecked = todo.done ? "checked" : "";
+  const { id, description, done } = todo;
 
   const content = `    
     <div class="view">
         <input class="toggle" type="checkbox" ${isChecked}>
-        <label>${todo.description}</label>
+        <label>${description}</label>
         <button class="destroy"></button>
     </div>
   `;
 
   const listElement = document.createElement("li");
   listElement.innerHTML = content;
-  listElement.setAttribute("data-id", todo.id);
+  listElement.setAttribute("data-id", id);
 
-  if (todo.done) listElement.classList.add("completed");
+  if (done) listElement.classList.add("completed");
 
   return listElement;
 };
--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,1 +1,2 @@
 export const TodoListCSSClass = ".todo-list";
+export const NewTodoInputCSSClass = "#new-todo-input";
