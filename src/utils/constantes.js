97a32dc336fcf6a5ee83a21dd4ad140f65c36680
Cesar Martinez <km.music92@gmail.com>
2023-09-07 15:49:20 -0500

    update: constantes

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,3 +1,13 @@
 export const TodoListCSSClass = ".todo-list";
 export const NewTodoInputCSSClass = "#new-todo-input";
 export const TodoListKey = "state";
+export const ClearAllCompletedId = ".clear-completed";
+export const FILTRO_TODOS = "Todos";
+export const FILTRO_PENDIENTES = "Pendientes";
+export const FILTRO_COMPLETADOS = "Completados";
+
+export const Filters = {
+  all: "All",
+  completed: "Completed",
+  pending: "Pending",
+};
