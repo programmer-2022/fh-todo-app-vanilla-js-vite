40057db95bf0c87b84deed37275e390e47415f5d
Cesar Martinez <km.music92@gmail.com>
2023-09-07 10:47:43 -0500

    update: add agregarListaTodosxDefecto function

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,75 +1,84 @@
 import { Todo } from "../todos/models/todo.model";
 
 const Filters = {
-  all: 'All',
-  completed: 'Completed',
-  pending: 'Pending'
-}
+  all: "All",
+  completed: "Completed",
+  pending: "Pending",
+};
 
 const state = {
-  todos: [
-    new Todo('Tarea 1'),
-    new Todo('Tarea 2'),
-    new Todo('Tarea 3')
-  ],
+  todos: [],
   filter: Filters.all,
-}
+};
 
 const initStore = () => {
-  console.log('InitStore', state);
-}
+  console.table(state.todos);
+};
 
 const loadStore = () => {
-  console.log('Load');
-}
+  console.log("Load");
+};
+
+const agregarListaTodosxDefecto = () => {
+  state.todos.push(
+    new Todo("Estudiar programación"),
+    new Todo("Crear un TODO usando React JS"),
+    new Todo("Agendar cita Médica"),
+    new Todo("Organizar ideas proyecto personal"),
+    new Todo("Crear un portfolio")
+  );
+  initStore();
+};
 
 const getTodos = (filter = Filters.all) => {
-  switch(filter) {
+  switch (filter) {
     case Filters.all:
       return [...state.todos];
     case Filters.completed:
-      return state.todos.filter(todo => todo.done);
+      return state.todos.filter((todo) => todo.done);
     case Filters.pending:
-      return state.todos.filter(todo => !todo.done);
+      return state.todos.filter((todo) => !todo.done);
     default:
       throw new Error(`Option ${filter} is not valid.`);
   }
-}
+};
 
 const addTodo = (description) => {
-  if(!description) throw new Error('Description is required');
+  if (!description) throw new Error("Description is required");
   state.todos.push(new Todo(description));
-}
+};
 
 const toggleTodo = (todoID) => {
+  const { todos } = state;
 
-  const index = state.todos.findIndex(todo => todo.id === todoID);
-    const findTodo = state.todos[index];
-    state.todos[index] = {
-        ...findTodo,
-        done: !findTodo.done,
-    }
-}
+  const index = todos.findIndex((todo) => todo.id === todoID);
+  const findTodo = todos[index];
+
+  todos[index] = {
+    ...findTodo,
+    done: !findTodo.done,
+  };
+};
 
 const deleteTodo = (todoID) => {
-  state.todos = state.todos.filter(todo => todo.id !== todoID);
-}
+  state.todos = state.todos.filter((todo) => todo.id !== todoID);
+};
 
 const deleteCompleted = () => {
-  state.todos = state.todos.filter(todo => todo.done);
-}
+  state.todos = state.todos.filter((todo) => todo.done);
+};
 
 const setFilter = (filter = Filters.all) => {
-  if(Filters[filter] === undefined)
+  if (Filters[filter] === undefined)
     throw new Error(`${filter} is not valid filter.`);
   state.filter = filter;
-}
+};
 
 const getCurrentFilter = () => {
   return state.filter;
-}
+};
 
-export default { 
+export default {
   initStore,
   loadStore,
   getTodos,
@@ -79,4 +88,5 @@ export default {
   deleteCompleted,
   setFilter,
   getCurrentFilter,
-}
+  agregarListaTodosxDefecto,
+};
